09da1dc691c2af5b7ed8093b737d883828bf0cd6
Hossen <mdmitho199911@gmail.com>
2022-04-22 14:35:32 +0600

    secure password on environmet variable

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port =process.env.PORT || 5000;
@@ -7,6 +8,18 @@ const app = express()
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c59qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Genius Car DB Connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req,res)=>{
     res.send('Running Genius Server')
